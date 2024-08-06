8c0453aacf650984d4a3a1b7ac77f0d715de8457
jyo1998 <jyothi.pulipati1998@gmail.com>
2024-08-05 22:47:50 -0500

    imported page component as all the 3 tabs are in this component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
-import MultilevelDropdown from './components/MultiLevelDropDown';
+import PageComponent from './components/PageComponent'; //As all the 3 tabs are rendering this page so we are calling this page
+
 
 const App = ()=> {
   return (
     <div className="App">
-  
-    < MultilevelDropdown />
-    
+     
+      <PageComponent />
+     
     </div>
   );
 }
