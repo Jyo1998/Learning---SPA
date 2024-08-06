03ed810e2055baa57e5b801df97135dde741dfbb
jyo1998 <jyothi.pulipati1998@gmail.com>
2024-08-05 22:52:50 -0500

    Added tab-3 data

---
+++ src/components/Tab.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box, Typography } from '@mui/material';
+
+
+const Tab3 = () => {
+    return(
+        <Box p={2}>
+          <Typography>This is the contents for Tab-3.</Typography>
+        </Box>
+    );
+}
+
+export default Tab3;
