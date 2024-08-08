c9f943d208ec987dda20486f2c13a3d6aa37b432
jyo1998 <jyothi.pulipati1998@gmail.com>
2024-08-07 22:28:10 -0500

    Added Data tab and Payment tab

--- src/components/PageComponent.js
+++ src/components/PageComponent.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import MultilevelDropdown from './MultiLevelDropDown';
 import FormComponent from './FormComponent';
 import Tab3 from './Tab';
+import DataTab from './dataTab';
+import PaymentTab from './PaymentTab';
 import { Tabs, Tab, Box, AppBar } from '@mui/material';
 
 const PageComponent = () => {
@@ -9,6 +11,8 @@ const PageComponent = () => {
   //setSelectedTab is the updater function that allows the user to change the value of selectedTab.
   const [address, setAddress] = useState('');//setAddress updates the state of address whenever the user changes it
   const [successMessage, setSuccessMessage] = useState('');
+  const [formData, setFormData] = useState({}); // Store form data
+
 
   const handleTabChange = (event, newValue) => { //intended to handle the changes in the selected tab such as clicking a different tab
     //newValue represents the new value of the selected tab and which tab should be selected after the change
@@ -20,9 +24,11 @@ const PageComponent = () => {
   };
 
   const handleFormSubmit = (formData) => { //represents the data from the form that is being submitted.
+    setFormData(formData); // store the form data
     setSuccessMessage('Form has been submitted successfully'); //state updater that modifies the successMessage state variable, which likely provides feedback to the user.
     // Navigate to Tab-3 after form submission
     setSelectedTab(2);
+
   };
 
   return (
@@ -34,6 +40,8 @@ const PageComponent = () => {
          <Tab label="Multilevel dropdown" /> {/*tab-1 name as it renders the multi level drop down component*/}
          <Tab label="Form data" /> {/*tab-2 name as it renders the form data */}
          <Tab label="Tab-3" />
+         <Tab label="Data Tab" />
+         <Tab label="Payment Tab" />
         </Tabs>
      </AppBar> 
       {selectedTab === 0 && ( //first tab is selected 
@@ -51,11 +59,22 @@ const PageComponent = () => {
       )}
     </Box>
      {successMessage && <div className="success-message">{successMessage}</div>}
+     {selectedTab === 3 && (
+      <Box p={2}>
+       <DataTab selectedAddress={address} onTabChange={handleTabChange} />
+       </Box>
+     )}
+     {selectedTab === 4 && (
+      <Box p={2} >
+        <PaymentTab formData={formData} />
+      </Box>
+     )
+     }
      </div>
     
   );
 };
 
-export default PageComponent;
+export default PageComponent; 
 
 
