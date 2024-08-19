393c1e1c3e0daf14761bb6df7f436428484c5c3f
jyo1998 <jyothi.pulipati1998@gmail.com>
2024-08-18 21:44:01 -0500

    Renders the underwriter component previously it renders the page component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import PageComponent from './components/PageComponent'; //As all the 3 tabs are rendering this page so we are calling this page
-
+//import PageComponent from './components/PageComponent'; //As all the 3 tabs are rendering this page so we are calling this page
+import UnderWriter from './UnderWriter/UnderWriterComponent';
 
 const App = ()=> {
   return (
     <div className="App">
      
-      <PageComponent />
+      <UnderWriter />
      
     </div>
   );
