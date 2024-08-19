0058063231caeec5670722813f6e6aa2c43cb734
jyo1998 <jyothi.pulipati1998@gmail.com>
2024-08-18 21:41:00 -0500

    Created a page for underwriter and all the tabs are rendering in it and it is the main component

---
+++ src/UnderWriter/UnderWriterComponent.js
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+import Placetab from "./Placetab";
+import FormatNumberTab from "./Formatnumbertab";
+import BookmarkTab from "./Bookmarktab";
+import ComparearrowTab from "./comparearrowtab";
+import { Tabs, Tab,  AppBar, Typography, Toolbar } from '@mui/material';
+import GroupDropdown from "./GroupDropdown";
+import './Page.css';
+
+const UnderWriter = () => {
+    const [selectedTab, setSelectedTab] = useState(0);
+    const handleTabChange = (event, newValue) => { 
+        setSelectedTab(newValue); 
+      };
+    return(               
+                    
+              <AppBar position="static" color="default">
+                <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Typography  variant="h6" noWrap>
+                     Guy Carpenter UnderWriter
+                     <GroupDropdown />
+                     </Typography>
+
+
+                  <Tabs textColor= { 'primary'} 
+                  value={selectedTab}   
+                  onChange={handleTabChange}
+                  >
+                  <Tab icon={<Placetab />} /> {/*tab-1 name as it renders the multi level drop down component*/}
+                  <Tab icon={<FormatNumberTab />} /> {/*tab-2 name as it renders the form data */}
+                  <Tab icon={<BookmarkTab />} />
+                  <Tab icon={<ComparearrowTab />} />
+                 </Tabs>
+
+                 
+                </Toolbar>
+                </AppBar>
+            
+
+                
+    )
+}
+
+export default UnderWriter;
