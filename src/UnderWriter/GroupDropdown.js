57d073adbc780c5fcc0a5c608b9989019a38d095
jyo1998 <jyothi.pulipati1998@gmail.com>
2024-08-19 12:33:40 -0500

    passing selected company as a prop

--- src/UnderWriter/GroupDropdown.js
+++ src/UnderWriter/GroupDropdown.js
@@ -1,52 +1,30 @@
-import React, {useState} from "react";
-import {
-    MenuItem,
-    FormControl,
-    Select,
-    InputLabel,
-    Button,
-} from '@mui/material'
+import React, { useState, useEffect } from 'react';
+import groupData from './group.json'; // Import group data
 
+const GroupDropdownTab = ({ selectedCompany }) => {
+  const [filteredGroups, setFilteredGroups] = useState([]);
 
-
-const GroupDropdown = ()=>{
-    const [selectedGroup, showSelectedGroup] = useState(null);
-    const [open, setOpen] = useState(false);
-    const handleChange = (event) => {
-        showSelectedGroup(event.target.value)
+  useEffect(() => {
+    if (selectedCompany) {
+      const company = groupData.companies.find(c => c.name === selectedCompany);
+      const groups = company ? company.group : [];
+      setFilteredGroups(groups);
+    } else {
+      setFilteredGroups([]);
     }
-    const handleClose = () => {
-        setOpen(false);
-      };
-    
-      const handleOpen = () => {
-        setOpen(true);
-      };
+  }, [selectedCompany]);
 
-    return(
-        <div>
-            <Button  onClick={handleOpen}>
-             Open the select
-            </Button>
-            <FormControl>
-        <InputLabel id="demo-controlled-open-select-label">Groups</InputLabel>
-        <Select
-          labelId="demo-controlled-open-select-label"
-          id="demo-controlled-open-select"
-          open={open}
-          onClose={handleClose}
-          onOpen={handleOpen}
-          value={selectedGroup}
-          onChange={handleChange}
-        >
-          <MenuItem value={10}>Hazard</MenuItem>
-          <MenuItem value={20}>List</MenuItem>
-          <MenuItem value={30}>HazardList</MenuItem>
-        </Select>
-      </FormControl>
-    
-      
-        </div>
-    )
-}
-export default GroupDropdown;
+  return (
+    <div>
+      <select>
+        {filteredGroups.map((group) => (
+          <option key={group.id} value={group.name}>
+            {group.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default GroupDropdownTab;
