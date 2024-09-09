609186f73159c3fe064e4af31aeb020ff57737c3
jyo1998 <jyothi.pulipati1998@gmail.com>
2024-09-09 12:38:08 -0500

    Removal of unnecessary code and addition of the data whenever the tabs are selected

--- src/UnderWriter/UnderWriterComponent.js
+++ src/UnderWriter/UnderWriterComponent.js
@@ -1,83 +1,3 @@
-// import React, {useState} from "react";
-// import Placetab from "./Placetab";
-// import FormatNumberTab from "./Formatnumbertab";
-// import BookmarkTab from "./Bookmarktab";
-// import ComparearrowTab from "./comparearrowtab";
-// import { Tabs, Tab,  AppBar, Typography, Toolbar, Box  } from '@mui/material';
-// import GroupDropdown from "./GroupDropdown";
-// import './Page.css';
-// import LayersDropdown from "./Layers";
-// import Location from "./Location";
-
-
-// const UnderWriter = () => {
-//     const [selectedTab, setSelectedTab] = useState(0);
-//     const [selectedCompany, setSelectedCompany] = useState('');
-    
-//     const handleTabChange = (event, newValue) => { 
-//         setSelectedTab(newValue); 
-//       };
-//       const handleSelectCompany = (company) => {
-//         setSelectedCompany(company);
-//       };
-//     return(               
-//                <div>     
-//               <AppBar position="static" color="default">
-//                 <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>
-//                 <Typography  variant="h6" noWrap>
-//                      Guy Carpenter UnderWriter
-                     
-//                      </Typography>
-                     
-//                      <GroupDropdown selectedCompany={selectedCompany}/>
-                     
-
-
-//                   <Tabs textColor= { 'primary'} 
-//                   value={selectedTab}   
-//                   onChange={handleTabChange}
-//                   indicatorColor="primary"
-//                   >
-//                   <Tab icon={<Placetab />} /> {/*tab-1 name as it renders the multi level drop down component*/}
-//                   <Tab icon={<FormatNumberTab />} /> {/*tab-2 name as it renders the form data */}
-//                   <Tab icon={<BookmarkTab />} />
-//                   <Tab icon={<ComparearrowTab onCompanySelect={handleSelectCompany}/>} />
-//                  </Tabs>
-                 
-        
-     
-                
-//                 </Toolbar>
-//                 </AppBar>
-                
-
-//                 <Box 
-//                 display="flex" 
-//                 flexDirection={'column'}
-//                 alignItems="flex-start" 
-//                 padding={2}
-//             >
-//                 <Box marginBottom={2}>
-//                   <Location /> 
-                    
-//                 </Box>
-//                 <Box>
-//                   <LayersDropdown /> 
-//                 </Box>
-//             </Box>
-//                 </div>
-
-            
-
-                
-//     )
-// }
-
-// export default UnderWriter;
-
-
-
-
 import React, { useState } from "react";
 import Placetab from "./Placetab";
 import FormatNumberTab from "./Formatnumbertab";
@@ -115,7 +35,7 @@ const UnderWriter = () => {
                         </Typography>
                         <Divider orientation="vertical" flexItem style={{ backgroundColor: '#ddd', marginRight: '16px' }} />
                         <GroupDropdown selectedCompany={selectedCompany} />
-                    </Box>
+                </Box>
                     <Tabs
                         textColor="primary"
                         value={selectedTab}
@@ -128,15 +48,34 @@ const UnderWriter = () => {
                         <Tab icon={<FormatNumberTab />} />
                         <Tab icon={<BookmarkTab />} />
                       
-                        
                         <Tab icon={<div>
-            <ComparearrowTab onCompanySelect={handleSelectCompany} />
-        </div> } />
-        </Tabs>
-                    
-                    
+                        <ComparearrowTab onCompanySelect={handleSelectCompany} />
+                        </div> } />
+                    </Tabs>  
                 </Toolbar>
             </AppBar>
+            <div>
+             {selectedTab === 0 && ( //first tab is selected 
+              <Box p={2}>
+                <Placetab />
+              </Box>
+              )}
+              {selectedTab === 1 && ( //second tab is selected 
+                <Box p={2}>
+                  <FormatNumberTab />
+                </Box>
+                )}
+                {selectedTab === 2 && ( //third tab is selected 
+                <Box p={2}>
+                  <BookmarkTab />
+                </Box>
+                )}
+                {selectedTab === 3 && ( //fourth tab is selected 
+                <Box p={2}>
+                  <ComparearrowTab onCompanySelect={handleSelectCompany} />
+                </Box>
+                )}
+            </div>
 
             <div style={{ display: 'flex', flexGrow: 1 }}>
                 {/* Sidebar (25% width) */}
@@ -151,8 +90,6 @@ const UnderWriter = () => {
                         </Box>
                     </Box>
                 </div>
-
-
             </div>
         </div>
     );
