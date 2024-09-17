9ffeec91568255f617f9c9c05c822b79625d7f1d
jyo1998 <jyothi.pulipati1998@gmail.com>
2024-09-17 11:36:12 -0500

    Added form data page as second page and underwriter as first page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 //import PageComponent from './components/PageComponent'; //As all the 3 tabs are rendering this page so we are calling this page
 import UnderWriter from './UnderWriter/UnderWriterComponent';
+import FormDataPage from "./UnderWriter/FormDataPage";
 
 const App = ()=> {
   return (
-    <div className="App">
-     
-      <UnderWriter />
-     
-    </div>
+    <Router>
+    <Routes>
+      {/* Main page route */}
+      <Route path="/" element={<UnderWriter />} />
+      {/* Route for form data page */}
+      <Route path="/form-data" element={<FormDataPage />} />
+    </Routes>
+  </Router>
   );
 }
 
