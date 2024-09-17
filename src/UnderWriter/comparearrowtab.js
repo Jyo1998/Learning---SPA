aa871113498434627765366518c9e41e86cfffcc
jyo1998 <jyothi.pulipati1998@gmail.com>
2024-09-17 11:36:53 -0500

    Adding of comments

--- src/UnderWriter/comparearrowtab.js
+++ src/UnderWriter/comparearrowtab.js
@@ -3,21 +3,24 @@ import CompareArrowsIcon from '@mui/icons-material/CompareArrows';
 import { Avatar, IconButton } from "@mui/material";
 import groupData from './group.json';
 
-
+//onCompanySelect - a prop passing from this to groupdropdown tab to show the groups which are related to the company selected by the user
 const ComparearrowTab = ({ onCompanySelect }) => {
   const [companies, setCompanies] = useState([]);
   const [showCompanies, setShowCompanies] = useState(false);
 
   useEffect(() => {
-      setCompanies(groupData.companies);
+      setCompanies(groupData.companies);//data from .json file
   }, []);
 
   const handleCompanyClick = (companyName) => {
       onCompanySelect(companyName);
   };
 
+  /*Initially showcompanies are false when the togglecompany button is clicked as the showcompanies is false
+  it sets to true and it shows the list of companies which are hidden and again it is clicked it is viceversa */
+/*Used to toggle the visibility of the company's list between showing and hiding it */
   const toggleCompanyList = () => {
-      setShowCompanies(!showCompanies);
+      setShowCompanies(!showCompanies); 
   };
 
   return (
